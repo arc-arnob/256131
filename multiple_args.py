5828ce975432280f77d706aef3dc9e68efc22606
Arnob Chowdhury <arnobchaudhury@gmail.com>
2021-04-18 08:10:08 +0530

    Added Regex Example

--- multiple_args.py
+++ multiple_args.py
@@ -24,12 +24,12 @@ if bar(1,2,3,magicnumber = 7) == True:
     print("Awesome!")
 
 #Ex 3
-def bar(first, second, third, **options):
+def bar_1(first, second, third, **options):
     if options.get("action") == "sum":
         print("The sum is: %d" %(first + second + third))
 
     if options.get("number") == "first":
         return first
 
-result = bar(1, 2, 3, action = "sum", number = "first")
+result = bar_1(1, 2, 3, action = "sum", number = "first")
 print("Result: %d" %(result))
